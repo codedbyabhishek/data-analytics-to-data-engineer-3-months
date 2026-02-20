0ccab5185a0ccbb550a400f3c7d618d7bf46ec06
Abhizerk <vtu16139@veltech.edu.in>
2026-02-20 15:31:53 +0530

    Configure live AWS stack outputs for ap-south-1

--- docs/aws-config.js
+++ docs/aws-config.js
@@ -1,6 +1,6 @@
 window.AWS_CONFIG = {
-  REGION: "",
-  USER_POOL_ID: "",
-  USER_POOL_CLIENT_ID: "",
-  API_BASE_URL: ""
+  REGION: "ap-south-1",
+  USER_POOL_ID: "ap-south-1_IYLYoMmMf",
+  USER_POOL_CLIENT_ID: "6vp0rgnf7m6a4qjo0bpnadu9hc",
+  API_BASE_URL: "https://okxmrkdtab.execute-api.ap-south-1.amazonaws.com"
 };
